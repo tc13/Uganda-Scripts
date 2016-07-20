6c24a02e71a96162875f746f9bcda71a0f506b95
tc13 <tomcrellen@gmail.com>
2016-07-20 11:38:12 +0100

    version 0.3 - uses less memory, but has to loop over input file twice

--- depth.bin.py
+++ depth.bin.py
@@ -8,52 +8,47 @@ file_in = argv[1]
 bin_size = int(argv[2])
 
 #Functions
-#Calculates max size of contig / chromosome
-def contig_size(chrom, depth_dict):
-        details = depth_dict.get(chrom)
-        return max(details.keys())
-
-#Creates 'bins' along contig
-def binner(chrom, depth_dict, bin_size):
-        lower = 0
-        upper = bin_size
-        max = contig_size(chrom, depth_dict)
-        #Total bins on contig
-	bin_num = max / bin_size
-        
-	for i in range(1, bin_num):
-		#looks up positions in the range (upper, lower) and finds the depth in depth_dict
-		l = [depth_dict.get(chrom).get(x) for x in range(lower,upper)]
-		#
-		try:
-			#Convert list elements to float
-			l2 = [float(x) for x in l]
-			#Print contig, bin number and mean depth in bin
-                        print chrom, "\t", i, "\t",  sum(l2) / float(bin_size)
-                except TypeError:
-                        pass
-		#Iteratively increase lower and upper values by bin size
-		lower += bin_size
-                upper += bin_size
-        
-#Read in file line by line -adding to depth dict
+#Read through file and calculate max size of contig - hold as dictionary
+size_dict = {}
 depth_dict = {}
 
 with open (file_in) as pileup:
-	for line in pileup:
-		#Extract 3 values from file - assumes 3 columns
-		try:
-			chrom, pos, depth = line.strip().split("\t")
-			depth_dict.setdefault(chrom, {})[int(pos)] = depth
-		except ValueError:
-			raise ValueError("Pileup Files does not have 3 columns")
+        for line in pileup:
+                #Extract 3 values - assumes 3 columns
+                try:
+                        chrom, pos, depth = line.strip().split("\t")
+   		except ValueError:
+                        raise ValueError("Pileup Files does not have 3 columns")
+                size_dict[chrom]= pos
 
-#Create unique set of keys (contigs)
-contig_set = set()
-for i in depth_dict.keys():
-	contig_set.add(i)
+Bin = 1
+clock = bin_size
+D = 0.0
 
-for i in contig_set:
-	binner(i, depth_dict, bin_size)
+with open (file_in) as pileup:
+        for line in pileup:
+                #Extract 3 values - assumes 3 columns
+                try:
+                        chrom, pos, depth = line.strip().split("\t")
+                except ValueError:
+                        raise ValueError("Pileup Files does not have 3 columns")
+
+		D += float(depth)
+		#print "The depth is  " +str(D)
+
+		if int(pos) == clock:
+			print chrom, "\t", Bin, "\t", float(D/bin_size)
+			Bin += 1
+			D = 0
+			clock += bin_size
+
+
+		#clock += 1
+		#print "The clock is "+ str(clock)
+
+		if size_dict.get(chrom) == pos:
+			Bin = 1
+			clock = bin_size		
+
 
 
