579c04e5621e9f1f06c2494f0006551963c8a605
tc13 <tomcrellen@gmail.com>
2016-07-20 13:00:09 +0100

    version 0.4 - uses less memory, 14MB for 6.5GB input, takes 19 mins, appears to work

--- depth.bin.py
+++ depth.bin.py
@@ -7,48 +7,40 @@ file_in = argv[1]
 #Size of bin is second argument
 bin_size = int(argv[2])
 
-#Functions
-#Read through file and calculate max size of contig - hold as dictionary
-size_dict = {}
-depth_dict = {}
+#set variables
+Bin =1 
+clock = bin_size
+D = 0.0
+tmp_chr = ""
 
+#Open file - read line by line
 with open (file_in) as pileup:
         for line in pileup:
-                #Extract 3 values - assumes 3 columns
+		#Strips values from 3 columns - will return error if input file is not 3 cols
                 try:
                         chrom, pos, depth = line.strip().split("\t")
    		except ValueError:
-                        raise ValueError("Pileup Files does not have 3 columns")
-                size_dict[chrom]= pos
+                        raise ValueError("Pileup File does not have 3 columns")
 
-Bin = 1
-clock = bin_size
-D = 0.0
-
-with open (file_in) as pileup:
-        for line in pileup:
-                #Extract 3 values - assumes 3 columns
-                try:
-                        chrom, pos, depth = line.strip().split("\t")
-                except ValueError:
-                        raise ValueError("Pileup Files does not have 3 columns")
-
-		D += float(depth)
-		#print "The depth is  " +str(D)
-
+		#This checks if chrom is new - in which case re-sets variables
+		if tmp_chr != chrom:
+                        tmp_chr = chrom
+                        Bin = 1
+                        D = 0.0
+                        clock = bin_size
+		
+		#Depth for the current bin stored in D 
+		D += int(depth)
+		
+		#If the position is equal to the bin_size - prints depth and moves onto next bin		
 		if int(pos) == clock:
 			print chrom, "\t", Bin, "\t", float(D/bin_size)
 			Bin += 1
-			D = 0
-			clock += bin_size
+                        D = 0
+                        clock += bin_size
+		
+	
 
-
-		#clock += 1
-		#print "The clock is "+ str(clock)
-
-		if size_dict.get(chrom) == pos:
-			Bin = 1
-			clock = bin_size		
 
 
 
